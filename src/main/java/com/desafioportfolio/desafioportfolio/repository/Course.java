0338539bf7e33fe569c445540fd524d2ad315d7e
matheustcolares <matheustcolares@gmail.com>
2023-10-25 18:37:58 -0300

    course CRUD impementation

--- src/main/java/com/desafioportfolio/desafioportfolio/repository/Course.java
+++
@@ -1,7 +0,0 @@
-package com.desafioportfolio.desafioportfolio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Course extends JpaRepository<Course, Long>{
-
-}
---
+++ src/main/java/com/desafioportfolio/desafioportfolio/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.desafioportfolio.desafioportfolio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desafioportfolio.desafioportfolio.model.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long>{
+
+}
